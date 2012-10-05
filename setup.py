44841e1c1fcaebaf03ba4bec0c31996962c79b13
Mad Penguin <madpenguin@linux.co.uk>
2012-10-05 17:56:44 +0100

    Path fixes

--- setup.py
+++ setup.py
@@ -9,9 +9,10 @@ setup(
 	version = "0.0.1",
 	description = ("Virtual Data Centre Cloud Platform"),
 	packages = ["VDCCloud"],
+	package_dir = {'VDCCloud':'VDCCloud'},
 	long_description = open("README.md").read(),
 	author = "Mad Penguin",
 	author_email = "madpenguin@linux.co.uk",
 	license = "GPLv3",
-	scripts = ["VDCCloud/fc_gluster"],
+	scripts = ["VDCCloud/fc_gluster","VDCCloud/fc_mongo.py"],
 	)
